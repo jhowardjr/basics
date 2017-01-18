1838649b705b3ce7af7c7d552f844da8928cf1df
Johnathan Howard <johnathan.howardjr@gmail.com>
2017-01-18 18:17:37 -0500

    remaining test for CArray

--- c/array_unittest.cc
+++ c/array_unittest.cc
@@ -24,7 +24,7 @@ TEST(CArrayTest, push_values)
 {
     for (int i = 0; i < capacity; i++)
     {
-	carray_push(array_ptr, i);
+        carray_push(array_ptr, i);
     }
 
     EXPECT_EQ(5, carray_get(array_ptr, 5));
@@ -48,9 +48,49 @@ TEST(CArrayTest, prepend_values)
 TEST(CArrayTest, pop_values)
 {
     int size = carray_size(array_ptr);
-    int last = carray_get(array_ptr, carray_size(array_ptr) - 1);
-    int second_last = carray_get(array_ptr, carray_size(array_ptr) - 2);
+    int last = carray_get(array_ptr, size - 1);
+    int second_last = carray_get(array_ptr, size - 2);
     EXPECT_EQ(last, carray_pop(array_ptr));
     EXPECT_EQ(second_last, carray_pop(array_ptr));
     EXPECT_EQ(size - 2, carray_size(array_ptr));
+}
+
+TEST(CArrayTest, delete_values)
+{
+    int size = carray_size(array_ptr);
+    int first = carray_get(array_ptr, 0);
+    int middle = carray_get(array_ptr, size / 2);
+    int last = carray_get(array_ptr, size - 1);
+    carray_delete(array_ptr, 0);
+    carray_delete(array_ptr, carray_size(array_ptr) / 2);
+    carray_delete(array_ptr, carray_size(array_ptr) - 1);
+
+    ASSERT_NE(first, carray_get(array_ptr, 0));
+    ASSERT_NE(middle, carray_get(array_ptr, size / 2));
+    ASSERT_NE(last, carray_get(array_ptr, size - 1));
+}
+
+TEST(CArrayTest, remove_values)
+{
+    int size = carray_size(array_ptr);
+    int first = carray_get(array_ptr, 0);
+    int middle = carray_get(array_ptr, size / 2);
+    int last = carray_get(array_ptr, size - 1);
+    carray_remove(array_ptr, first);
+    carray_remove(array_ptr, middle);
+    carray_remove(array_ptr, last);
+    ASSERT_EQ(-1, carray_find(array_ptr, first));
+    ASSERT_EQ(-1, carray_find(array_ptr, middle));
+    ASSERT_EQ(-1, carray_find(array_ptr, last));
+}
+
+TEST(CArrayTest, find_values)
+{
+    int size = carray_size(array_ptr);
+    int first = carray_get(array_ptr, 0);
+    int middle = carray_get(array_ptr, size / 2);
+    int last = carray_get(array_ptr, size - 1);
+    ASSERT_LE(0, carray_find(array_ptr, first));
+    ASSERT_LE(size / 2, carray_find(array_ptr, middle));
+    ASSERT_LE(size - 1, carray_find(array_ptr, last));
 }
